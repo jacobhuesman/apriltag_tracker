1a2ed7420c0c8b72ba4da78c73fb8f9cde7aba41
jacobhuesman <jacob.huesman@ndsu.edu>
2018-01-10 16:14:33 -0600

    Added tf publishing and additional timing info

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,3 @@
-/*Program for the senior design node
-1. Captures an image from a USB camera,
-2. Pipes it into apriltags.
-3. Then finds the transform from the camera to the tag
-   and extracts the angle from this transform
-4. Tells the servo to move to this angle through
-   an overdamped PI compensator (for now,
-   will later need to be type 2 system.)
-5. Sends transform from tag to camera X,Y,theta
-   over CAN in a format
-*/
-
 #include <iostream>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
@@ -21,12 +9,17 @@
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Geometry>
 
-#include <chrono> // for cross platform waiting
+#include <chrono>
 #include <thread>
 
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <tf/transform_broadcaster.h>
+
+#include <apriltag_tracker/AprilTagDetection.h>
+#include <apriltag_tracker/AprilTagDetectionArray.h>
 
 #include <raspicam/raspicam.h>
 
@@ -38,7 +31,7 @@ typedef struct {
   float py;
 } CameraConfig;
 
-Eigen::Matrix4f getRelativeTransform(CameraConfig cfg, double tag_size, const cv::Point2f tagPts[]);
+Eigen::Matrix4f getRelativeTransform(CameraConfig cfg, float tag_size, const cv::Point2f tagPts[]);
 void draw(cv::Mat& image, const cv::Point2f p[], at::Point cxy, size_t id);
 
 
@@ -99,6 +92,8 @@ int main(int argc, char **argv)
   /* ROS */
   ros::init(argc, argv, "apriltag_detector");
   ros::NodeHandle nh;
+  tf::TransformBroadcaster tf_pub;
+  ros::Publisher detections_pub = nh.advertise<apriltag_tracker::AprilTagDetection>("tag_detections", 1);
   image_transport::ImageTransport it(nh);
   image_transport::Publisher image_pub = it.advertise("tag_detections_image", 1);
 
@@ -113,6 +108,7 @@ int main(int argc, char **argv)
   std::chrono::milliseconds calc_time[10];
   long running_count = 0;
   long iterations = 0;
+
 
   /*
    * Start
@@ -132,6 +128,7 @@ int main(int argc, char **argv)
     calc_point[0] = std::chrono::system_clock::now();
     //camera >> captured_img.image;
     camera.grab();
+    ros::Time capture_time = ros::Time::now();
     calc_point[1] = std::chrono::system_clock::now();
     camera.retrieve(captured_img.image.data);
     calc_point[2] = std::chrono::system_clock::now();
@@ -145,36 +142,76 @@ int main(int argc, char **argv)
     // Output data
     //std::cout << detections.size() << " tags detected" << std::endl;
     //TODO Add tag sizes
+    apriltag_tracker::AprilTagDetectionArray tag_detection_array;
+    for (int i = 0; i < detections.size(); i++)
+    {
+      // Get transform
+      /* Transform */
+      Eigen::Matrix4f transform = getRelativeTransform(cam0_config, tag_size, detections[i].p);
+      Eigen::Matrix3f rotation = transform.block(0, 0, 3, 3);
+      Eigen::Quaternion<float> rotation_q = Eigen::Quaternion<float>(rotation);
+
+      // Build tag
+      geometry_msgs::PoseStamped tag_pose;
+      tag_pose.pose.position.x = transform(0, 3);
+      tag_pose.pose.position.y = transform(1, 3);
+      tag_pose.pose.position.z = transform(2, 3);
+      tag_pose.pose.orientation.x = rotation_q.x();
+      tag_pose.pose.orientation.y = rotation_q.y();
+      tag_pose.pose.orientation.z = rotation_q.z();
+      tag_pose.pose.orientation.w = rotation_q.w();
+      tag_pose.header.frame_id ="camera";
+      tag_pose.header.stamp = capture_time;
+      tag_pose.header.seq = 0; // TODO make into sequence
+
+      apriltag_tracker::AprilTagDetection tag_detection;
+      tag_detection.pose = tag_pose;
+      tag_detection.id = detections[i].id;
+      tag_detection.size = tag_size;
+      tag_detection_array.detections.push_back(tag_detection);
+
+      // Create transform
+      tf::Stamped<tf::Transform> tag_transform;
+      tf::poseStampedMsgToTF(tag_pose, tag_transform);
+      tf_pub.sendTransform(tf::StampedTransform(tag_transform, tag_transform.stamp_, tag_transform.frame_id_, "cam0"));
+    }
+
+    calc_point[5] = std::chrono::system_clock::now();
+
     for (int i = 0; i < detections.size(); i++)
     {
       draw(captured_img.image, detections[i].p, detections[i].cxy, detections[i].id);
     }
-    //getRelativeTransform(cam0_config, tag_size, detections[i].p);
 
-
-    calc_point[5] = std::chrono::system_clock::now();
-    image_pub.publish(captured_img.toImageMsg());
     calc_point[6] = std::chrono::system_clock::now();
 
-    //calc_point[2] = std::chrono::system_clock::now();
-    calc_time[0] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[1] - calc_point[0]);
-    calc_time[1] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[2] - calc_point[1]);
-    calc_time[2] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[3] - calc_point[2]);
-    calc_time[3] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[4] - calc_point[3]);
-    calc_time[4] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[5] - calc_point[4]);
-    calc_time[5] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[6] - calc_point[5]);
-    calc_time[6] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[6] - calc_point[0]);
+    image_pub.publish(captured_img.toImageMsg());
+    calc_point[7] = std::chrono::system_clock::now();
+
+    //calc_poinsetw(4)t[2] = std::chrono::system_clock::now();
+    calc_time[0] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[1] - calc_point[0]); // Grab
+    calc_time[1] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[2] - calc_point[1]); // Retrieve
+    calc_time[2] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[3] - calc_point[2]); // Grayscale
+    calc_time[3] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[4] - calc_point[3]); // Process
+    calc_time[4] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[5] - calc_point[4]); // Get transform
+    calc_time[5] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[6] - calc_point[5]); // Draw tag on image
+    calc_time[6] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[7] - calc_point[6]); // Publish image
+    calc_time[7] = std::chrono::duration_cast<std::chrono::milliseconds>(calc_point[7] - calc_point[0]); // Total time
 
 
-
-    running_count += calc_time[6].count();
+    running_count += calc_time[7].count();
     iterations++;
 
-    std::cout << "Grab: " << calc_time[0].count() << ", retrieve: " << calc_time[1].count();
-    std::cout << ", grayscale: " << calc_time[2].count() << ", process: " << calc_time[3].count();
-    std::cout << ", publish: " << calc_time[5].count() << ", total: " << calc_time[6].count();
-    std::cout << ", draw: " << calc_time[4].count();
-    std::cout << ", average total time: " << running_count / iterations << std::endl;
+    std::cout << "Grab: "           << std::setw(2) << calc_time[0].count();
+  //std::cout << ", retrieve: "     << std::setw(2) << calc_time[1].count();
+  //std::cout << ", grayscale: "    << std::setw(2) << calc_time[2].count();
+    std::cout << ", process: "      << std::setw(2) << calc_time[3].count();
+    std::cout << ", pub_trans: "    << std::setw(2) << calc_time[4].count();
+  //std::cout << ", draw: "         << std::setw(2) << calc_time[5].count();
+    std::cout << ", pub_img: "      << std::setw(2) << calc_time[6].count();
+    std::cout << ", total: "        << std::setw(2) << calc_time[7].count();
+    std::cout << ", avg_tot_time: " << std::setw(2) << running_count / iterations;
+    std::cout << std::endl;
 
     //std::string image_path = "/home/nrmc/ws/test_images/image" + std::to_string(iterations) + ".jpg";
     //cv::imwrite(image_path, captured_image_mat);
@@ -183,15 +220,15 @@ int main(int argc, char **argv)
   #pragma clang diagnostic pop
 }
 
-Eigen::Matrix4f getRelativeTransform(CameraConfig cfg, double tag_size, const cv::Point2f tagPts[])
+Eigen::Matrix4f getRelativeTransform(CameraConfig cfg, float tag_size, const cv::Point2f tagPts[])
 {
   std::vector<cv::Point3f> objPts;
   std::vector<cv::Point2f> imgPts;
-  double s = tag_size/2.;
-  objPts.push_back(cv::Point3f(-s,-s, 0));
-  objPts.push_back(cv::Point3f( s,-s, 0));
-  objPts.push_back(cv::Point3f( s, s, 0));
-  objPts.push_back(cv::Point3f(-s, s, 0));
+  float s = tag_size/2.0f;
+  objPts.emplace_back(cv::Point3f(-s,-s, 0));
+  objPts.emplace_back(cv::Point3f( s,-s, 0));
+  objPts.emplace_back(cv::Point3f( s, s, 0));
+  objPts.emplace_back(cv::Point3f(-s, s, 0));
 
   imgPts.push_back(tagPts[0]);
   imgPts.push_back(tagPts[1]);
@@ -205,14 +242,14 @@ Eigen::Matrix4f getRelativeTransform(CameraConfig cfg, double tag_size, const cv
       0,           0,      1);
   cv::Vec4f distParam(0,0,0,0); // all 0?
   cv::solvePnP(objPts, imgPts, cameraMatrix, distParam, rvec, tvec);
-  cv::Matx33d r;
+  cv::Matx33f r;
   cv::Rodrigues(rvec, r);
   Eigen::Matrix3f wRo;
   wRo << r(0,0), r(0,1), r(0,2), r(1,0), r(1,1), r(1,2), r(2,0), r(2,1), r(2,2);
 
   Eigen::Matrix4f T;
   T.topLeftCorner(3,3) = wRo;
-  T.col(3).head(3) << tvec.at<double>(0), tvec.at<double>(1), tvec.at<double>(2);
+  T.col(3).head(3) << tvec.at<float>(0), tvec.at<float>(1), tvec.at<float>(2);
   T.row(3) << 0,0,0,1;
 
   return T;
