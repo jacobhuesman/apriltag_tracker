fdb2b0c7a2e8489a7da9c7c80000d3333ee958e2
jacobhuesman <jacob.huesman@ndsu.edu>
2018-02-13 14:47:09 -0600

    Fixed tests

--- src/tests/transform_tests.cpp
+++ src/tests/transform_tests.cpp
@@ -26,27 +26,29 @@ TEST(AprilTagTrackerTransformTests, ConstructorAndGetters)
   ASSERT_NEAR(6.0, test.getServoTf().getOrigin().getZ(), 1e-10);
   ASSERT_NEAR(M_PI_2, test.getTagTheta(), 1e-10);
 }
-/*
+
 TEST(AprilTagTrackerTransformTests, LessThanOperator)
 {
-  tf2::Stamped<tf2::Transform> tf1;
-  tf2::Quaternion q1;
-  q1.setRPY(0.0, 0.0, M_PI_2);
-  tf1.setRotation(q1);
-  Transform test1(tf1);
+  tf2::Stamped<tf2::Transform> servo_tf;
 
-  tf2::Stamped<tf2::Transform> tf2;
-  q1.setRPY(0.0, 0.0, M_PI_2 * 1.1);
-  tf2.setRotation(q1);
-  Transform test2(tf2);
+  tf2::Stamped<tf2::Transform> tag_tf1;
+  tf2::Quaternion q;
+  q.setRPY(0.0, M_PI_4, 0.0);
+  tag_tf1.setRotation(q);
+  Transform test1(tag_tf1, servo_tf);
 
-  ASSERT_NEAR(M_PI_2, test1.getTagTheta(), 1e-10);
-  ASSERT_NEAR(M_PI_2 * 1.1, test2.getTagTheta(), 1e-10);
+  tf2::Stamped<tf2::Transform> tag_tf2;
+  q.setRPY(0.0, M_PI_4 * 1.1, 0.0);
+  tag_tf2.setRotation(q);
+  Transform test2(tag_tf2, servo_tf);
+
+  ASSERT_NEAR(M_PI_4, test1.getTagTheta(), 1e-10);
+  ASSERT_NEAR(M_PI_4 * 1.1, test2.getTagTheta(), 1e-10);
 
   ASSERT_TRUE(test1 < test2);
   ASSERT_FALSE(test2 < test1);
   ASSERT_FALSE(test1 < test1);
-}*/
+}
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv)
